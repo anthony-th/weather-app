e2eb568692db8acfc752cf97504e526db3475f35
Anthony <yefremov.anthony@gmail.com>
2023-02-13 14:59:24 +0600

    refactor: add simple framer-motion

--- src/components/WeatherFive.jsx
+++ src/components/WeatherFive.jsx
@@ -1,55 +1,64 @@
 import React from "react";
 import { IMG_URL } from "../shared/utils";
+import { motion } from "framer-motion";
+import { weatherItem } from "../shared/utils";
+import { weatherContainer } from "../shared/utils";
 
 const WeatherFive = (props) => {
   const { fiveDaysWeather } = props;
 
   return (
     <>
-     {fiveDaysWeather.list && (
-          <div className="flex flex-wrap justify-center text-gray-600 gap-2 max-w-[320px] sm:max-w-full">
-            {Object.values(
-              fiveDaysWeather.list.reduce((acc, day) => {
-                const date = new Date(day.dt * 1000).toLocaleDateString();
-                acc[date] = acc[date] || {
-                  date,
-                  ...day.main,
-                  weather: day.weather[0],
-                };
-                return acc;
-              }, {})
-            ).map((day, index) => (
-              <div
-                key={index}
-                className="shadow-lg flex flex-col py-4 px-3 bg-[#92afd9c2] rounded-lg text-black/75 items-center"
-              >
-                <p className="text-center text-xl font-medium">{day.date}</p>
-                <div className="text-center">
-                  <img
-                    className="w-[70px] h-[70px]"
-                    src={`${IMG_URL}/${day.weather.icon}@2x.png`}
-                    alt={day.weather.description}
-                  />
-                  <p className="text-center text-xl font-medium">
-                    {Math.trunc(day.temp)}°C
-                  </p>
-                </div>
-                <p className="text-sm font-medium">
-                  Feels like: {Math.trunc(day.feels_like)}°C
-                </p>
-                <p className="text-center text-smfont-medium">
-                  {day.weather.description}
-                </p>
-                <p className="text-center text-sm font-medium">
-                  Humidity: {day.humidity}
-                </p>
-                <p className="text-center text-sm font-medium">
-                  Pressure: {day.pressure}hPa
+      {fiveDaysWeather.list && (
+        <motion.div
+          className="flex flex-wrap justify-center text-gray-600 gap-2 max-w-[320px] sm:max-w-full"
+          variants={weatherContainer}
+          initial="hidden"
+          animate="visible"
+        >
+          {Object.values(
+            fiveDaysWeather.list.reduce((acc, day) => {
+              const date = new Date(day.dt * 1000).toLocaleDateString();
+              acc[date] = acc[date] || {
+                date,
+                ...day.main,
+                weather: day.weather[0],
+              };
+              return acc;
+            }, {})
+          ).map((day, index) => (
+            <motion.div
+              variants={weatherItem}
+              key={index}
+              className="min-w-[138px] shadow-lg flex flex-col py-4 px-3 bg-[#92afd9c2] rounded-lg text-black/75 items-center"
+            >
+              <p className="text-center text-xl font-medium">{day.date}</p>
+              <div className="text-center">
+                <img
+                  className="w-[70px] h-[70px]"
+                  src={`${IMG_URL}/${day.weather.icon}@2x.png`}
+                  alt={day.weather.description}
+                />
+                <p className="text-center text-xl font-medium">
+                  {Math.trunc(day.temp)}°C
                 </p>
               </div>
-            ))}
-          </div>
-        )}
+              <p className="text-sm font-medium">
+                Feels like: {Math.trunc(day.feels_like)}°C
+              </p>
+              <p className="text-center text-smfont-medium">
+                {day.weather.description}
+              </p>
+              <p className="text-center text-sm font-medium">
+                Humidity: {day.humidity}
+              </p>
+              <p className="text-center text-sm font-medium">
+                Pressure: {day.pressure}hPa
+              </p>
+            </motion.div>
+          ))}
+        </motion.div>
+      )}
     </>
   );
 };
--- src/shared/utils.js
+++ src/shared/utils.js
@@ -1,3 +1,26 @@
 export const API_KEY = '';
 export const API_URL = 'https://api.openweathermap.org/data/2.5';
 export const IMG_URL = 'https://openweathermap.org/img/wn';
+export const weatherContainer = {
+  hidden: { opacity: 1 },
+  visible: {
+    opacity: 1,
+    transition: {
+      delayChildren: 0.7,
+      staggerChildren: 0.2,
+      duration: 0.5
+    }
+  }
+};
+export const weatherItem = {
+  hidden: { y: 20, opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      delayChildren: 0.7,
+      staggerChildren: 0.2,
+      duration: 0.5
+    }
+  }
+};
