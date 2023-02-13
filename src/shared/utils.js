58d5c03e045f80459ab39d21248627816328ae6b
Anthony <yefremov.anthony@gmail.com>
2023-02-13 13:14:00 +0600

    refactor: del api

--- src/shared/utils.js
+++ src/shared/utils.js
@@ -1,3 +1,3 @@
-export const API_KEY = '91d2dd3e0546bacc1b50edc59b6c0069';
+export const API_KEY = '';
 export const API_URL = 'https://api.openweathermap.org/data/2.5';
 export const IMG_URL = 'https://openweathermap.org/img/wn';
